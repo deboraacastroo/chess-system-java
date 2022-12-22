6c17175a64b80706da64972292b309321b0e36a6
Debora Castro <debora_castro_96@hotmail.com>
2022-12-22 15:39:10 +0000

    Handling exceptions and clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,7 +46,6 @@ public class ChessMatch {
 		}
 	}
 	
-	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
